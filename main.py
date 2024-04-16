cf0f6101cd2b6f7d8175009e967a4f7dcf56c223
Manuel Dominguez <65312866+Manuel-Dominguez-ds@users.noreply.github.com>
2024-04-16 11:33:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from Pipelines.Trainer import *
 if __name__=='__main__':
     trainer = Trainer('Data/train.csv')
     trainer.orchestrator()
-    # Crear scorer class y hacer predicciones para test data
+    scorer=Scorer('Data/test.csv')
+    scorer.orchestrator()
